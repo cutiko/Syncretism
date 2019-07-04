e6a0a39c49a60d806dad7501edfcef0ad7f504f2
cutiko <cutiko@gmail.com>
2019-07-04 17:01:10 -0400

    ADD: basic login

--- App.tsx
+++ App.tsx
@@ -1,21 +1,61 @@
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import Auth0 from 'react-native-auth0';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android: "live reload"
-});
 
 //TODO https://manage.auth0.com/dashboard/us/cutiko/applications/VoGh1xi1OtJbQx87wc2Ip0Ylnpq0qK99/quickstart
 //TODO https://github.com/auth0-samples/auth0-react-native-sample/blob/Embedded/01-Custom-Form/app/components/Login.js
-type Props = {};
-export default class App extends Component<Props> {
+//TODO update IOS
+//TODO debugger https://github.com/jhen0409/react-native-debugger coolest debugger
+interface Props{};
+
+interface State {
+  login: string | null
+}
+class Login extends React.Component<Props, State>{
+
+  constructor(props : Props) {
+    super(props)
+    this.state = {
+      login: null
+    }
+  }
+}
+
+//test@test.com
+//12345678Aa
+
+
+export default class App extends React.Component<Props, State> {
+
+  constructor(props : Props) {
+      super(props)
+      this.state = {
+          login: null
+      }
+      this.login()
+  }
+
+
+  login : Function = async ()=> {
+    const auth0 = new Auth0({domain: 'cutiko.auth0.com', clientId: 'VoGh1xi1OtJbQx87wc2Ip0Ylnpq0qK99'})
+    try {
+      const result = await auth0
+        .webAuth
+        .authorize({scope: 'openid profile email', audience: 'https://cutiko.auth0.com/userinfo'})
+      this.setState({login:JSON.stringify(result)})
+    } catch (e) {
+      this.setState({login:JSON.stringify(e)})
+    }
+    console.log(this.state.login)
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <ScrollView>
+          <Text style={styles.welcome}>{this.state.login}</Text>
+        </ScrollView>
       </View>
     );
   }
