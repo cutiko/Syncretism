e0b5304ab6099a58e2cf7a8a45c21a9e6ead3edb
cutiko <cutiko@gmail.com>
2019-07-05 16:15:34 -0400

    ADD: welcome screen appearance

--- component/WelcomeScreen.tsx
+++ component/WelcomeScreen.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
-import {View} from "react-native";
+import React, {Component} from 'react'
+import {Text, TouchableOpacity, View} from "react-native";
 import styles from "../theme/styles";
-import {accent} from "../theme/colors";
 
-export default ()=>{
-  return (<View style={[styles.container, {backgroundColor: accent}]}/>)
+export default class WelcomeScreen extends Component {
+  static navigationOptions = {header: null}
+
+  render() {
+    return (
+      <View style={[styles.container, styles.center, styles.padding]}>
+        <Text style={[styles.title, styles.padding]}>Syncretism</Text>
+        <Text style={[styles.secondaryText, styles.padding]}>
+          En este demo hay React Native, con componentes nativos de Android y IOS, además entre éstos últimos hay Kotlin
+          compartido
+        </Text>
+        <TouchableOpacity style={[styles.accentBtn, styles.padding, {marginTop: 16}]}>
+          <Text style={styles.whiteText}>Siguiente</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
 }
