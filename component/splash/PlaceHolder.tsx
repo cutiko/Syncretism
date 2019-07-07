5afad8d898f71267734efff6da8f0b62f83c082d
cutiko <cutiko@gmail.com>
2019-07-07 10:32:16 -0400

    REMOVE: first attempt from component

--- component/splash/PlaceHolder.tsx
+++ component/splash/PlaceHolder.tsx
@@ -7,15 +7,6 @@ import PorterDuff from '../../native/android/PorterDuff'
 
 export default ({callback}: {callback?: Function})=> {
 
-  //if (callback) setTimeout(callback(), 1200)
-  const image = require('../../images/syncretism_logo.png')
-  console.log("image", image);
-  console.log("to string", image.toString());
-
-  const imageNo = require('image!../../images/syncretism_logo.png')
-  console.log("imageNo", imageNo);
-  PorterDuff.compose(null, null, image)
-
 
   return(
     <View style={[styles.container, styles.center, {backgroundColor: primaryDark}]}>
