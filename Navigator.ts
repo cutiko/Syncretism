52017efac940d7d98df9b1524a7937b6867f1779
cutiko <cutiko@gmail.com>
2019-07-08 09:58:26 -0400

    ADD: demo screen navigation

--- Navigator.ts
+++ Navigator.ts
@@ -1,12 +1,15 @@
 import {createStackNavigator, NavigationRouteConfigMap} from "react-navigation";
 import SplashScreen from "./component/splash/SplashScreen";
 import WelcomeScreen from "./component/WelcomeScreen";
+import DemoScreen from "./component/demo/DemoScreen";
 
 export const HOME_SCREEN = "HOME_SCREEN"
 export const WELCOME_SCREEN = "WELCOME_SCREEN"
+export const DEMO_SCREEN = "DEMO_SCREEN"
 
 const map : NavigationRouteConfigMap = {}
 map[HOME_SCREEN] = {screen: SplashScreen}
 map[WELCOME_SCREEN] = {screen: WelcomeScreen}
+map[DEMO_SCREEN] = {screen: DemoScreen}
 
 export default createStackNavigator(map)
--- component/WelcomeScreen.tsx
+++ component/WelcomeScreen.tsx
@@ -1,9 +1,17 @@
 import React, {Component} from 'react'
 import {Text, TouchableOpacity, View} from "react-native";
 import styles from "../theme/styles";
+import {Props as AppProps} from '../types/component'
+import {DEMO_SCREEN} from "../Navigator";
 
-export default class WelcomeScreen extends Component {
+interface Props extends AppProps{}
+
+export default class WelcomeScreen extends Component<Props> {
   static navigationOptions = {header: null}
+
+  goToDemo = ()=> {
+    this.props.navigation.navigate(DEMO_SCREEN)
+  }
 
   render() {
     return (
@@ -13,7 +21,9 @@ export default class WelcomeScreen extends Component {
           En este demo hay React Native, con componentes nativos de Android y IOS, además entre éstos últimos hay Kotlin
           compartido
         </Text>
-        <TouchableOpacity style={[styles.accentBtn, styles.padding, {marginTop: 16}]}>
+        <TouchableOpacity
+          style={[styles.accentBtn, styles.padding, {marginTop: 16}]}
+          onPress={()=>this.goToDemo()}>
           <Text style={styles.whiteText}>Siguiente</Text>
         </TouchableOpacity>
       </View>
