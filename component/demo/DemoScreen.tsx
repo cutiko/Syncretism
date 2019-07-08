07b7bb77031878b37b531093472524cb2f6224a4
cutiko <cutiko@gmail.com>
2019-07-08 10:06:40 -0400

    ADD: android demo is working

--- component/demo/DemoScreen.tsx
+++ component/demo/DemoScreen.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
-import {View} from "react-native";
+import {Platform, Text, View} from "react-native";
 import styles from "../../theme/styles";
+import PorterDuffView from "../../native/android/PorterDuffView";
 
 export default () => {
+
+  if ((Platform.OS === 'ios')) {
+    return (
+      <View style={[styles.container, {backgroundColor: 'orange'}]}>
+        <Text>IOS</Text>
+      </View>
+    )
+  }
+
   return (
-    <View style={[styles.container, {backgroundColor: 'orange'}]}/>
+    <View style={[styles.container, styles.center, styles.padding]}>
+      <Text style={[styles.title, styles.padding]}>PorterDuffView</Text>
+      <Text style={[styles.padding, styles.secondaryText]}>Componente nativo de Android. Es un ImageView que utiliza PorterDuff para mezclar dos imagenes. Las imagenes son dynámicas a partir de 2 urls</Text>
+      <PorterDuffView
+        style={{width: 200, height: 200}}
+        urls={{
+          destination: "https://i.ibb.co/ZYH76ZT/circle.png",
+          source: "https://i.ibb.co/6yJNR7K/square.png"
+        }}
+      />
+    </View>
   )
 }
