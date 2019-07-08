3c59b3cd66b0b7d9f0ff333dbbb7d18f36690d66
cutiko <cutiko@gmail.com>
2019-07-08 09:46:16 -0400

    IMPROVE: rn asset pipeline instead of condition

--- component/splash/LoginFail.tsx
+++ component/splash/LoginFail.tsx
@@ -1,17 +1,16 @@
 import React from 'react'
-import {Image, Platform, Text, TouchableOpacity, View} from "react-native";
+import {Image, Text, TouchableOpacity, View} from "react-native";
 import styles from "../../theme/styles";
 import {primaryLight} from "../../theme/colors";
 
 
 export default ({callback}: {callback : Function})=> {
-  const img = (Platform.OS === 'ios') ? require('../../images/apple_error.png') : require('../../images/android_error.png')
   return(
     <View style={[styles.container, styles.center, {backgroundColor: primaryLight}]}>
       <Text style={[styles.title, styles.whiteText]}>
         Inicie Sesión Por Favor
       </Text>
-      <Image source={img}/>
+      <Image source={require('../../images/error.png')}/>
       <Text style={[styles.secondaryText, styles.padding]}>
         El inicio de sesión fue cancelado o la sesión expiró
       </Text>
